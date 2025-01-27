ba8033b37c5ef7e6f389346b7b21afbe51b32b70
Madison <madison.bullard@gmail.com>
2025-01-27 15:44:50 -0500

    Remove unused code in native app

--- packages/native/app/index.tsx
+++ packages/native/app/index.tsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import { Button, H1, H3, Input, isWeb, Paragraph, SizableText, XStack, YStack } from 'tamagui'
+import { Button, H1, SizableText, XStack, YStack } from 'tamagui'
 import { authClient, useAuth } from '~/better-auth/client'
 import { Avatar } from '~/interface/Avatar'
-import { randomID } from '~/zero/randomID'
-import { useQuery,  } from '~/zero/zero'
 
 export function HomePage() {
-  // const [messages] = useQuery((q) => q.message.orderBy('createdAt', 'desc'))
   const { user } = useAuth()
-  const [text, setText] = useState('')
-  const existingUser = useQuery((q) => q.user)[0][0]
 
   return (
     <YStack
@@ -43,52 +37,6 @@ export function HomePage() {
           Login with Github
         </Button>
       )}
-
-      <YStack w="100%" gap="$4" p="$4" bc="$color2" bw={1} br="$4">
-        <Input onChangeText={setText} />
-
-        <Button
-          onPress={async () => {
-            const existing = user?.id || existingUser?.id
-            let userId = existing || randomID()
-
-            console.log('PRESS', existing || 'NO USER')
-
-            // if (!existing) {
-            //   // for now just insert some random user!
-            //   await zero.mutate.user.insert({
-            //     id: userId,
-            //     email: '',
-            //     image: '',
-            //     username: 'firstuser',
-            //     name: 'First User',
-            //     state: {},
-            //     updatedAt: new Date().getTime(),
-            //     createdAt: new Date().getTime(),
-            //   })
-            // }
-
-            // zero.mutate.message.insert({
-            //   id: randomID(),
-            //   senderId: userId,
-            //   content: text,
-            //   createdAt: new Date().getTime(),
-            // })
-          }}
-        >
-          Post
-        </Button>
-      </YStack>
-
-      <H3>Messages</H3>
-{/* 
-      {messages.map((message) => {
-        return (
-          <YStack key={message.id}>
-            <Paragraph>{message.content}</Paragraph>
-          </YStack>
-        )
-      })} */}
     </YStack>
   )
 }
--- packages/native/src/zero/zero.ts
+++ packages/native/src/zero/zero.ts
@@ -1,11 +1,5 @@
-import {
-	type Query,
-	type QueryType,
-	type Smash,
-	type TableSchema,
-	Zero,
-} from "@rocicorp/zero";
-import { useZero, useQuery as useZeroQuery } from "@rocicorp/zero/react";
+import { Zero } from "@rocicorp/zero";
+import { useZero as useZeroBase } from "@rocicorp/zero/react";
 import { createEmitter } from "@vxrn/emitter";
 import { type Schema, schema } from "core/zero/index";
 
@@ -37,25 +31,6 @@ export function setZeroAuth({
 	zeroEmitter.emit(zero);
 }
 
-export type QueryResult<TReturn extends QueryType> = [
-	Smash<TReturn>,
-	{
-		type: "unknown" | "complete";
-	},
-];
-
-export function useQuery<
-	T extends TableSchema,
-	QueryBuilder extends (z: Zero<Schema>["query"]) => Query<T>,
-	Q extends ReturnType<QueryBuilder>,
->(
-	createQuery: QueryBuilder,
-): Q extends Query<T, infer Return> ? QueryResult<Return> : never {
-	const z = useZero<Schema>();
-	return useZeroQuery(createQuery(z.query)) as Q extends Query<
-		T,
-		infer Return extends QueryType
-	>
-		? QueryResult<Return>
-		: never;
+export function useZero() {
+	return useZeroBase<Schema>();
 }
