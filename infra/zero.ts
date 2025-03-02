f7b6dcb3b97207eeb36fc008311aa2b9fffc7e59
Madison <madison.bullard@gmail.com>
2025-03-02 09:50:05 -0500

    rm unused

--- infra/zero.ts
+++ infra/zero.ts
@@ -1,5 +1,4 @@
 import { readFileSync } from "node:fs";
-import { join } from "node:path";
 import { cluster } from "./cluster";
 import { dbProperties, postgres } from "./database";
 import { secret } from "./secret";
